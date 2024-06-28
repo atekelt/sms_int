fec4be8210fc9df7c3e352e80d1d7e7a2e913507
atekelt <atekeltafework55@gmail.com>
2024-06-28 17:22:17 +0300

    Commit for push

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const smppClient = require('./smppClient');
+
+const app = express();
+app.use(bodyParser.json());
+
+smppClient.connect();
+
+app.get('/', (req,res) => {
+    res.send("SMS Integration Server");
+  });
+
+app.post('/subscribe', (req, res) => {
+  const { userID, spID, productID, serviceID } = req.body;
+
+  const message = `Subscription request: UserID=${userID}, SPID=${spID}, ProductID=${productID}, ServiceID=${serviceID}`;
+
+  smppClient.sendSMS(userID, message, (err, pdu) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.status(200).json({ message: 'Subscription message sent', pdu });
+    }
+  });
+});
+
+app.post('/unsubscribe', (req, res) => {
+  const { userID, spID, productID, serviceID } = req.body;
+
+  const message = `Unsubscription request: UserID=${userID}, SPID=${spID}, ProductID=${productID}, ServiceID=${serviceID}`;
+
+  smppClient.sendSMS(userID, message, (err, pdu) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.status(200).json({ message: 'Unsubscription message sent', pdu });
+    }
+  });
+});
+
+module.exports = app;
---
+++ smppClient.js
@@ -0,0 +1,51 @@
+const smpp = require('smpp');
+require('dotenv').config();
+let session;
+
+const connect = () => {
+  session = new smpp.Session({host: process.env.HOST, port: process.env.PORT});
+
+  session.on('connect', () => {
+    session.bind_transceiver({
+      system_id: process.env.USER,
+      password: process.env.PASS
+    }, (pdu) => {
+      if (pdu.command_status === 0) {
+        console.log('Successfully bound to SMPP server');
+      }
+    });
+  });
+
+  session.on('close', () => {
+    console.log('SMPP session closed');
+  });
+
+  session.on('error', (err) => {
+    console.error('SMPP session error:', err);
+  });
+};
+
+const sendSMS = (phoneNumber, message, callback) => {
+  if (!session) {
+    callback(new Error('No SMPP session established'));
+    return;
+  }
+
+  session.submit_sm({
+    destination_addr: phoneNumber,
+    short_message: message
+  }, (pdu) => {
+    if (pdu.command_status === 0) {
+      console.log('Message sent successfully');
+      callback(null, pdu);
+    } else {
+      console.error('Failed to send message:', pdu);
+      callback(new Error('Failed to send message'));
+    }
+  });
+};
+
+module.exports = {
+  connect,
+  sendSMS
+};
